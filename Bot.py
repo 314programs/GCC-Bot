958dcb98f5fe73a735bb803a6f9edb17e1d7a1f4
314programs <314programs@gmail.com>
2024-01-31 23:32:44 +0800

    Piano tile player bot(for testing)

--- Bot.py
+++ Bot.py
@@ -3,5 +3,40 @@ import pyautogui
 import time
 import keyboard
 import random
-import win32api, win32con
+import objc
+from Quartz.CoreGraphics import (
+    CGEventCreateMouseEvent, 
+    kCGEventMouseMoved, 
+    kCGEventLeftMouseDown, 
+    kCGEventLeftMouseUp, 
+    kCGMouseButtonLeft, 
+    CGEventPost, 
+    kCGHIDEventTap, 
+    CGPointMake
+)
 
+def move_mouse(x, y):
+    mouse_event = CGEventCreateMouseEvent(None, kCGEventMouseMoved, CGPointMake(x, y), kCGMouseButtonLeft)
+    CGEventPost(kCGHIDEventTap, mouse_event)
+
+def click(x, y):
+    move_mouse(x, y)
+    mouse_event_down = CGEventCreateMouseEvent(None, kCGEventLeftMouseDown, CGPointMake(x, y), kCGMouseButtonLeft)
+    CGEventPost(kCGHIDEventTap, mouse_event_down)
+
+    time.sleep(0.01)
+    
+    mouse_event_up = CGEventCreateMouseEvent(None, kCGEventLeftMouseUp, CGPointMake(x, y), kCGMouseButtonLeft)
+    CGEventPost(kCGHIDEventTap, mouse_event_up)
+
+while keyboard.is_pressed('q') == False:
+    print(pyautogui.pixel(581, 400))
+    if pyautogui.pixel(581, 400)[0] == 0:
+        click(581, 400)
+    if pyautogui.pixel(682, 400)[0] == 0:
+        click(682, 400)
+    if pyautogui.pixel(770, 400)[0] == 0:
+        click(770, 400)
+    if pyautogui.pixel(869, 400)[0] == 0:
+        click(869, 400)
+    time.sleep(5)
